980a91bcf7b26d19732303adccc1f220ca55099f
Doma <leishenghao@126.com>
2020-04-17 10:54:08 +0800

    improvement(SelectPicker): 添加 export SelectPickerProps

--- src/SelectPicker.tsx
+++ src/SelectPicker.tsx
@@ -1,4 +1,14 @@
 import { SelectPicker as RSuiteSelectPicker } from 'rsuite';
-import enhancePicker from './HOCs/enhancePicker';
+import { SelectProps } from 'rsuite/es/SelectPicker';
+import enhancePicker, { EnhancedPickerProps } from './HOCs/enhancePicker';
+import { FormControlPickerProps } from "rsuite/es/@types/common";
+
+// 修复 SelectPickerProps
+export interface SelectPickerProps<ValueType = any>
+  extends FormControlPickerProps<ValueType>,
+    SelectProps<ValueType>,
+    EnhancedPickerProps {
+
+}
 
 export default enhancePicker(RSuiteSelectPicker);
